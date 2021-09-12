3e5e18166e1029caaeed4bda791e2e80ca45d37b
Patrícia Assunção <patriciaapassuncao@gmail.com>
2021-09-12 20:49:32 -0300

    Create Thirteenth Exercise.py

---
+++ Thirteenth Exercise.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 12 19:01:46 2021
+
+@author: Patricia
+"""
+
+print= ("Hello, World")
+
+#Exercício com operadores aritmeticos em Python
+
+numero_w = 7
+numero_x = 6
+numero_y = 4
+numero_z = 3
+
+#soma
+
+soma = numero_w + numero_y + numero_z + numero_x
+print (soma) 
+
+#subtracao
+
+subtracao = numero_w - numero_y - numero_z - numero_x
+print (subtracao)
+
+#exponenciacao
+
+exponenciacao = numero_x ** numero_y
+print (exponenciacao)
+
+exponenciacao= numero_y ** numero_x
+print(exponenciacao)
+
+exponenciacao = numero_w ** numero_z
+print(exponenciacao)
+
+exponenciacao = numero_z ** numero_w
+print (exponenciacao)
+
+exponenciacao= numero_z ** numero_x
+print(exponenciacao)
+
+exponenciacao = numero_y ** numero_z
+print(exponenciacao)
+
+exponenciacao = numero_z ** numero_w
+print (exponenciacao)
+
+
+#multiplicacao
+
+multiplicacao = numero_x * numero_y
+print= (multiplicacao)
+
+multiplicacao = numero_x * numero_x
+print= (multiplicacao)
+
+multiplicacao = numero_y * numero_y
+print= (multiplicacao)
+
+multiplicacao = numero_w * numero_z
+print= (multiplicacao)
+
+multiplicacao = numero_w * numero_w
+print= (multiplicacao)
+
+multiplicacao = numero_z * numero_z
+print= (multiplicacao)
+
+multiplicacao = numero_x * numero_w
+print= (multiplicacao)
+
+multiplicacao = numero_z * numero_z
+print= (multiplicacao)
+
+#divisao
+
+divisao = numero_x / numero_y
+print= (divisao)
+
+divisao = numero_x / numero_x
+print= (divisao)
+
+divisao = numero_y / numero_y
+print= (divisao)
+
+divisao = numero_w / numero_z
+print= (divisao)
+
+divisao = numero_w / numero_w
+print= (divisao)
+
+divisao = numero_z / numero_z
+print= (divisao)
+
+divisao = numero_x / numero_w
+print= (divisao)
+
+divisao = numero_z / numero_z
+print= (divisao)
+
+#divisao inteira
+
+divisao_inteira = numero_x // numero_y
+print= (divisao)
+
+divisao_inteira = numero_x // numero_x
+print= (divisao)
+
+divisao_inteira = numero_y // numero_y
+print= (divisao)
+
+divisao_inteira = numero_w // numero_z
+print= (divisao)
+
+divisao_inteira = numero_w // numero_w
+print= (divisao)
+
+divisao_inteira = numero_z // numero_z
+print= (divisao)
+
+divisao_inteira = numero_x // numero_w
+print= (divisao)
+
+divisao_inteira = numero_z // numero_z
+print= (divisao)
+
+#modulo
+
+modulo = numero_x % numero_y
+print = (modulo)
+
+modulo = numero_w % numero_z
+print = (modulo)
+
+modulo = numero_y % numero_z
+print = (modulo)
+
+modulo = numero_x % numero_w
+print = (modulo)
+
